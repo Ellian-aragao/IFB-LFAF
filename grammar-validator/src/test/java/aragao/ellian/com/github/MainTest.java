adae1f0153e7b8e5c588b29c8f90a66f4001cd78
Ellian Aragão Dias <ellian.aragao@gmail.com>
2023-10-13 21:24:27 -0300

    test(trabalho1): adicionando caso de teste

--- grammar-validator/src/test/java/aragao/ellian/com/github/MainTest.java
+++ grammar-validator/src/test/java/aragao/ellian/com/github/MainTest.java
@@ -3,13 +3,25 @@ package aragao.ellian.com.github;
 import org.junit.jupiter.api.Test;
 
 import static aragao.ellian.com.github.Main.verificarGramatica;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MainTest {
 
     @Test
     void validateCasebaba() {
         String palavra = "baba";
+        assertTrue(verificarGramatica(palavra));
+    }
+
+    @Test
+    void validateCaseagua() {
+        String palavra = "agua";
+        assertTrue(verificarGramatica(palavra));
+    }
+
+    @Test
+    void validateCasecanto() {
+        String palavra = "canto";
         assertTrue(verificarGramatica(palavra));
     }
 }
